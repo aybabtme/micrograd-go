9aef258c8138795be6c8ff3b456f5c41904543a8
Antoine Grondin <antoinegrondin@gmail.com>
2024-03-17 14:04:30 +0900

    init

---
+++ main.go
@@ -0,0 +1,47 @@
+//go:build ignore
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"os/exec"
+
+	mg "github.com/aybabtme/micrograd"
+	"github.com/pkg/browser"
+)
+
+func main() {
+	a := mg.New(2.0, "a")
+	b := mg.New(-3.0, "b")
+	c := mg.New(10.0, "c")
+	e := a.Mul(b, "e")
+	d := e.Add(c, "d")
+	f := mg.New(-2.0, "f")
+	L := d.Mul(f, "L")
+
+	dot := bytes.NewBuffer(nil)
+	mg.DotGraph(L, dot)
+
+	log.Printf(dot.String())
+
+	if err := openDot(dot); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func openDot(r io.Reader) error {
+	cmd := exec.Command("dot", "-Tsvg")
+	cmd.Stdin = r
+	data, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("turning dot into svg: %v", err)
+	}
+	svg := bytes.NewBuffer(data)
+	if err := browser.OpenReader(svg); err != nil {
+		return fmt.Errorf("opening browser: %v", err)
+	}
+	return nil
+}
---
+++ micrograd.go
@@ -0,0 +1,110 @@
+package micrograd
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"math"
+)
+
+type scalar = float64
+
+type Value struct {
+	data     scalar
+	grad     scalar
+	prev     [2]*Value
+	op       string
+	label    string
+	backward func()
+}
+
+func New(data scalar, label string) Value {
+	return Value{
+		data:  data,
+		label: label,
+	}
+}
+
+func (self Value) Add(other Value, label string) Value {
+	out := Value{
+		data:  self.data + other.data,
+		prev:  [2]*Value{&self, &other},
+		op:    "+",
+		label: label,
+	}
+	out.backward = func() {
+		self.grad = 1.0 * out.grad
+		other.grad = 1.0 * out.grad
+	}
+	return out
+}
+
+func (self Value) Mul(other Value, label string) Value {
+	out := Value{
+		data:  self.data * other.data,
+		prev:  [2]*Value{&self, &other},
+		op:    "*",
+		label: label,
+	}
+	out.backward = func() {
+		self.grad = other.data * out.grad
+		other.grad = self.data * out.grad
+	}
+	return out
+}
+
+func (self Value) Tanh(label string) Value {
+	x := self.data
+	t := math.Tanh(x)
+	out := Value{
+		data:  t,
+		prev:  [2]*Value{&self, nil},
+		op:    "tanh",
+		label: label,
+	}
+	out.backward = func() {
+		self.grad = (1 - math.Pow(t, 2)) * out.grad
+	}
+	return out
+}
+
+func (self Value) String() string {
+	return fmt.Sprintf("Value(data=%f)", self.data)
+}
+
+func (self *Value) dotvisit(parent *Value, parentname string, nodes, edges *bytes.Buffer) {
+	name := fmt.Sprintf("%p", &self)
+	fmt.Fprintf(nodes, "\t"+`%q [label="%s | data %.4f | grad %.4f", shape=record]`+"\n", name, self.label, self.data, self.grad)
+
+	linkname := name
+	if self.op != "" {
+		opname := name + self.op
+		fmt.Fprintf(nodes, "\t%q [label=%q]\n", name+self.op, self.op)
+		fmt.Fprintf(edges, "\t%q -> %q /* link from op %q to value %q */\n", opname, name, self.op, self.label)
+		linkname = opname
+	}
+
+	if parent != nil {
+		fmt.Fprintf(edges, "\t%q -> %q /* parent link to %q */\n", name, parentname, parent.label)
+	}
+
+	for _, prev := range self.prev {
+		if prev != nil {
+			prev.dotvisit(self, linkname, nodes, edges)
+		}
+	}
+}
+
+func DotGraph(v Value, out io.Writer) (int, error) {
+	nodes := bytes.NewBuffer(nil)
+	edges := bytes.NewBuffer(nil)
+
+	fmt.Fprintf(nodes, "digraph {\n\trankdir=LR\n")
+	v.dotvisit(nil, "", nodes, edges)
+
+	edges.WriteTo(nodes)
+
+	fmt.Fprintf(nodes, "}\n")
+
+	return out.Write(nodes.Bytes())
+}
